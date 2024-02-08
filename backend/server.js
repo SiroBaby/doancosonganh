ddbb7926c0c9a4af34f7513b8bb0a88fbe6ea451
SiroBaby <ngocphat076@gmail.com>
2024-02-08 13:35:32 +0700

    them server backend

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,18 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-app.post('/checkout', (req, res) => {
-  const { ten_sanpham, gia_ban, soluong, thanhtien } = req.body;
-  const sql = 'INSERT INTO sanpham (ten_sanpham, gia_ban, soluong, thanhtien) VALUES (?, ?, ?, ?)';
-  db.query(sql, [ten_sanpham, gia_ban, soluong, thanhtien], (err, result) => {
-    if (err) {
-      console.error('Error executing query: ' + err.stack);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    res.status(200).send('Payment successful');
-  });
-});
+// app.post('/checkout', (req, res) => {
+//   const { ten_sanpham, gia_ban, soluong, thanhtien } = req.body;
+//   const sql = 'INSERT INTO sanpham (ten_sanpham, gia_ban, soluong, thanhtien) VALUES (?, ?, ?, ?)';
+//   db.query(sql, [ten_sanpham, gia_ban, soluong, thanhtien], (err, result) => {
+//     if (err) {
+//       console.error('Error executing query: ' + err.stack);
+//       res.status(500).send('Internal Server Error');
+//       return;
+//     }
+//     res.status(200).send('Payment successful');
+//   });
+// });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
