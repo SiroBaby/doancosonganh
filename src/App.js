75d5957331a51a3a25e642c25ad8351713ad2eda
lamnhune <2254810052@vaa.edu.vn>
2024-03-01 13:57:17 +0700

    LN ne

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
 import Addproducts from './components/addproducts';
 import Adminpage from './components/adminpage';
@@ -9,6 +9,7 @@ import Userlist from './components/userlist';
 import Editproducts from './components/Editproducts';
 import Editpicture from './components/Editpicture';
 import Trangchu from './components/Trangchu';
+import ProductDetail from './components/ProductDetail';
 const App = () => {
   return (
     <Router>
@@ -16,13 +17,14 @@ const App = () => {
         {/* <Route path="/home" Component={Home} />
         <Route path="/" Component={Login} /> 
         <Route path="/giohang" Component={GH} /> */}
-        <Route path='/' Component={Login}/>
-        <Route path='/addproducts' Component={Addproducts}/>
-        <Route path='/admin' Component={Adminpage}/>
-        <Route path='/user' Component={Userlist}/>
-        <Route path='/editproducts/:id' Component={Editproducts}/>
-        <Route path='/editpicture/:id' Component={Editpicture}/>
-        <Route path='/trangchu' Component={Trangchu}/>
+        <Route path='/' Component={Login} />
+        <Route path='/addproducts' Component={Addproducts} />
+        <Route path='/admin' Component={Adminpage} />
+        <Route path='/user' Component={Userlist} />
+        <Route path='/editproducts/:id' Component={Editproducts} />
+        <Route path='/editpicture/:id' Component={Editpicture} />
+        <Route path='/trangchu' Component={Trangchu} />
+        <Route path="/product-detail/:id" Component={ProductDetail} />
       </Routes>
     </Router>
 
